8c6b45e8fe9017cf89d8351d3d971a22970efc72
Thabo <thabochambule1@gmail.com>
2024-08-20 16:39:44 +0200

    Build html calculator skeleton

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+let num1;
+let opp;
+let num2;
+
+
+function add(){
+
+}
+function substract(){
+
+}
+function multiply(){
+
+
+}
+function divide(){
+
+}
+
+function operate(num1,opp,num2){
+    add()
+
+}
