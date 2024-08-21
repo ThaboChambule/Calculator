efc4ee59b9e2b13ba0b5481c90cb4b385573c145
Thabo <thabochambule1@gmail.com>
2024-08-21 22:38:24 +0200

    app operator functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,63 @@
-let value1 = '';
-let opp = '';
-let value2 = '';
+let value1 = "";
+let opp = "";
+let value2 = "";
 
+display();
+
+function display(){
 document.querySelectorAll(".number").forEach((button) => {
   button.addEventListener("click", (event) => {
     const number = parseInt(event.target.getAttribute("data-value"));
     const display = document.querySelector(".js-display");
-    
+
+    if(opp===""){
+      value1 += number;
+      display.value = value1;
+    }
+    else{
+      value2 += number;
+      display.value = value2;
+    }
 
 
 
+    display.value += number;
   });
 });
+
+document.querySelectorAll(".operator").forEach((button) => {
+  button.addEventListener("click", (event) => {
+    const opp = event.target.getAttribute("data-value");
+  });
+});
+
+document.querySelector(".equal").addEventListener("click", () => {
+  let result;
+  switch (opp) {
+    case "+":
+      result = add(parseFloat(value1), parseFloat(value2));
+      break;
+    case "-":
+      result = substract(parseFloat(value1), parseFloat(value2));
+      break;
+    case "x":
+      result = multiply(parseFloat(value1), parseFloat(value2));
+      break;
+    case "÷":
+      result = divide(parseFloat(value1), parseFloat(value2));
+      break;
+  }
+
+  
+  
+  
+});
+
+}
+
+
+
+
 
 function add(value1, value2) {
   return value1 + value2;
